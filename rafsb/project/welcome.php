24ac9ac877dbf91ec5a5f2344b12e6777b130da7
Rafael <rafael@aboxsoft.com.br>
2018-10-08 17:17:49 -0300

    upd

---
+++ rafsb/project/welcome.php
@@ -0,0 +1,43 @@
+<section class="bar tct bwhite">
+	<article class="bar tct">
+		<div class="xpd">
+			<div class="-map@-wd30vw lt xyp">
+				<row class="fb flt tct">WE'VE GOT</row>
+				<row class="fg tct">1.000.000</row>
+				<row class="fb flt tct">LIVES SAVED</row>
+				<row class="flt tct">AND COUNTING</row>
+			</div>
+			<div class="-map@-wd30vw lt">
+				<img class="wf" src='src/img/worldmap.png'/>
+			</div>
+		</div>
+	</article>
+	<nav class="bar tct fdark">
+		<div>
+			<div class="lt tct xyp -map@-wd20vw cur">
+				<icon class="fg fvariant flt">&#xe107;</icon>
+				<div class="zbar tct fn">SIGN UP</div>
+				<div class="spd">Join us! Let's make the world a better place to live! Together we can!</div>
+			</div>
+			<div class="lt tct xyp -map@-wd20vw cur">
+				<icon class="fg fvariant flt">&#xe0e9;</icon>
+				<div class="zbar tct fn">SAVING LIFES</div>
+				<div class="spd">Millions are saved around the world, just sharing some hashtags!</div>
+			</div>
+			<div class="lt tct xyp -map@-wd20vw cur">
+				<icon class="fg fvariant flt">&#xe01f;</icon>
+				<div class="zbar tct fn">MAP OF FACTS</div>
+				<div class="spd">Find out where people are enslaved or children are sold!</div>
+			</div>
+		</div>
+	</nav>
+	<script>
+		{{%#}} = {
+			init: function(){
+				this.container.appear();
+				ab.loading(AB_OFF);
+				ab.organize();
+			}
+		}
+	</script>
+</section>
